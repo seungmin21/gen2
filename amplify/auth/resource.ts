587e7758420d3b41e613e3f9d755245295a06eba
ohseungmin <ohmin2886@gmail.com>
2024-01-24 13:17:25 +0900

    재 실험 커밋

---
+++ amplify/auth/resource.ts
@@ -0,0 +1,35 @@
+import { defineAuth } from '@aws-amplify/backend';
+
+/**
+ * Define and configure your auth resource
+ * When used alongside data, it is automatically configured as an auth provider for data
+ * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ */
+export const auth = defineAuth({
+  loginWith: {
+    email: {
+      verificationEmailSubject: 'Welcome! Verify your email!'
+   },
+    // add social providers
+    externalProviders: {
+    }
+  }
+});
+  /**
+   * enable multifactor authentication
+   * @see https://docs.amplify.aws/gen2/build-a-backend/auth/manage-mfa
+   */
+  // multifactor: {
+  //   mode: 'OPTIONAL',
+  //   sms: {
+  //     smsMessage: (code) => `Your verification code is ${code}`,
+  //   },
+  // },
+  userAttributes: {
+    /** request additional attributes for your app's users */
+    // profilePicture: {
+    //   mutable: true,
+    //   required: false,
+    // },
+  },
+});
